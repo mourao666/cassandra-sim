77af71d2fb12d0aee8fd8be094dde2b5eb7599c2
mourao666 <mourao666@lntb-mul037628.network.ctbc>
2015-11-15 17:18:33 -0200

    Initial test of getToken method

--- test/unit/org/apache/cassandra/dht/SimilarityPartitionerInitialTest.java
+++ test/unit/org/apache/cassandra/dht/SimilarityPartitionerInitialTest.java
@@ -1,9 +1,13 @@
 package org.apache.cassandra.dht;
 
+import java.nio.ByteBuffer;
+import java.util.BitSet;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import org.apache.cassandra.db.marshal.BinaryType;
+import org.apache.cassandra.utils.ObjectSizes;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -11,11 +15,43 @@ import static org.junit.Assert.assertTrue;
 public class SimilarityPartitionerInitialTest
 {
     private IPartitioner partitioner;
+    private ByteBuffer key;
+    private BitSet keyHash;
 
     @Before
-    public void initPartitioner()
+    public void init()
+    {
+        initPartitioner();
+        initKey();
+        initKeyHash();
+    }
+
+    private void initPartitioner()
     {
         partitioner = SimilarityPartitioner.instance;
+    }
+
+    private void initKey()
+    {
+        double[] object = {10.0, 5.0, 6.0, 1.0, 0.0, 2.0};
+        key = ByteBuffer.allocate(8 * object.length);
+        for (int i = 0; i < object.length; i++) {
+            key.putDouble(object[i]);
+        }
+        key.rewind();
+    }
+
+    private void initKeyHash()
+    {
+        keyHash = new BitSet(8);
+        keyHash.set(0, true);
+        keyHash.set(1, false);
+        keyHash.set(2, true);
+        keyHash.set(3, false);
+        keyHash.set(4, true);
+        keyHash.set(5, true);
+        keyHash.set(6, true);
+        keyHash.set(7, false);
     }
 
     @Test
@@ -39,7 +75,22 @@ public class SimilarityPartitionerInitialTest
     @Test
     public void testGetToken() throws Exception
     {
+        assertTrue(partitioner.getToken(ByteBuffer.wrap(new byte[]{})).isMinimum());
+        testToken(partitioner.getToken(key));
+    }
 
+    private void testToken(Token token)
+    {
+        // compareTo
+        assertEquals(-1, partitioner.getMinimumToken().compareTo(new SimilarityPartitioner.BinaryToken(keyHash)));
+        assertEquals(0, token.compareTo(new SimilarityPartitioner.BinaryToken(keyHash)));
+        assertEquals(1, token.compareTo(partitioner.getMinimumToken()));
+        // getPartitioner
+        assertEquals(partitioner, token.getPartitioner());
+        // getHeapSize
+        assertEquals(ObjectSizes.measure(new SimilarityPartitioner.BinaryToken(new BitSet())) +
+                     ObjectSizes.sizeOfArray(keyHash.toByteArray()), token.getHeapSize());
+        // getTokenValue
     }
 
     @Test
